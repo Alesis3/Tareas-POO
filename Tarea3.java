2f11b3e2850e965e02fafbb5b73c0f74a4c6d269
Alexis Art <113313380+Alesis3@users.noreply.github.com>
2023-02-22 21:27:08 -0600

    Add files via upload

---
+++ Tarea3.java
@@ -0,0 +1,11 @@
+public class Tarea3 {
+    public static void main(String[] args)
+    {
+    System.out.println("-----");
+    System.out.println("|   |");
+    System.out.println("|   |");
+    System.out.println("|   |");
+    System.out.println("-----");
+
+    }
+}
